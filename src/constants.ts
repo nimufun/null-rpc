4de601cd729d9323d94f433451844295e0417bcf
0xeabz <eabz@kindynos.mx>
2025-12-27 20:02:36 -0600

    Add more nodes

--- src/constants.ts
+++ src/constants.ts
@@ -9,6 +9,13 @@ export const CHAIN_NODES: Record<ChainId, string[]> = {
     'https://rpc.mevblocker.io',
     'https://rpc.flashbots.net',
     'https://cloudflare-eth.com',
-    'https://singapore.rpc.blxrbdn.com'
+    'https://singapore.rpc.blxrbdn.com',
+    'https://ethereum-rpc.publicnode.com',
+    'https://singapore.rpc.blxrbdn.com',
+    'https://eth.rpc.blxrbdn.com',
+    'https://eth-mainnet.public.blastapi.io',
+    'https://eth-mainnet.rpcfast.com?api_key=xbhWBI1Wkguk8SNMu1bvvLurPGLXmgwYeC4S6g2H7WdwFigZSmPWVZRxrskEQwIf',
+    'https://eth.meowrpc.com',
+    'https://ethereum-mainnet.gateway.tatum.io'
   ]
 }
